c7952cd82a0b19970214a74de7ec683a62779417
fdila <federicadilauro1998@gmail.com>
2020-09-12 13:42:01 +0200

    remove useless stuff

--- MDK-ARM/PROFILER.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del "OUTPUTFILE.TXT"
-UV4.exe -b %1 -j0 -o"OUTPUTFILE.TXT"
-echo.
-echo ================================================================ BUILD LOG ================================================================
-type OUTPUTFILE.TXT
